049f3fd534ea6b15352d010e457437191e843678
Nicci Tud <77980531+NicciTud@users.noreply.github.com>
2026-02-27 17:48:34 +0100

    Added initial falcon_input node with headers and functions
    Functions still need to be defined, and publishers need to be added

---
+++ falcon_input.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import rospy
+from geometry_msgs.msg import PoseStamped, WrenchStamped, Point, Vector3
+from sensor_msgs.msg import Joy
+import numpy as np
+import time
+
+class FalconInput:
+    def __init__(self):
+        rospy.init_node('falcon_input')
+        #publishers here
+
+
+        def init_pub():
+
+        def get_button_state():
+        def get_position():  
+        def detect_release():
+        def calculate_pull_vector():
+        def get_pull_distance():
+        def publish_panda_command(vector):
+        
