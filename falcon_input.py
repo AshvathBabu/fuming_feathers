a8dee8e8212edafd4151578b5ae26a9cf36d08fa
Group 6 <r3dhattar@gmail.com>
2026-05-08 19:38:06 +0200

    Changed Twist to TwistStamped
    This change was used to accomodate the code in slingshot_sim.py

--- falcon_input.py
+++ falcon_input.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import rospy
-from geometry_msgs.msg import PoseStamped, WrenchStamped, Twist
+from geometry_msgs.msg import PoseStamped, WrenchStamped, TwistStamped
 from sensor_msgs.msg import Joy
 import numpy as np
 import time
@@ -14,7 +14,7 @@ class falcon:
         #publisher
         self.pub = rospy.Publisher('falcon/servo_cf', WrenchStamped, queue_size=1)
             #for panda Twist messages
-        self.velocity_pub = rospy.Publisher('/fuming_feathers/velocity_cmd', Twist, queue_size=1)
+        self.velocity_pub = rospy.Publisher('/fuming_feathers/velocity_cmd', TwistStamped, queue_size=1)
         #subscribers
         self.position_sub = rospy.Subscriber('falcon/measured_cp', PoseStamped, self.get_position)
         self.joy_sub = rospy.Subscriber('falcon/joy', Joy, self.joy_callback)
@@ -30,7 +30,7 @@ class falcon:
 
 #main loop
     def main(self): 
-        r= rospy.Rate(500)
+        r= rospy.Rate(20)
 
         while not rospy.is_shutdown() and self.position is None: #sleep mode
             r.sleep()
@@ -108,17 +108,18 @@ class falcon:
 
         rospy.loginfo("X = %f \n Y = %f \n Z = %f", x,y,z)
 
-        return (x, y, z)
+        return (z, -x, y)
 
 #added, give vector to panda, panda file will check limits
     def publish_panda_command(self, vector):
-        twist = Twist()
+        twist = TwistStamped()
+        twist.header.frame_id = "panda_hand_tcp"
         scale = 2.0 #multiply with distance to get vel, so this is 0.2 m/s for 10cm
 
         #set twist linear velocity fields x,y,z
-        twist.linear.x = vector[0] * scale
-        twist.linear.y = vector[1] * scale
-        twist.linear.z = vector[2] * scale
+        twist.twist.linear.x = vector[0] * scale
+        twist.twist.linear.y = vector[1] * scale
+        twist.twist.linear.z = vector[2] * scale
         
         self.velocity_pub.publish(twist)
         rospy.loginfo("Published velocity to Panda")
